ebbc569a7b52a562eacd8b0cc74b313d18ba759f
irfanhidayatullah <irfanhidayatullah.jobs@gmail.com>
2024-07-17 22:59:59 +0700

    update v4.4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,13 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL (process.env.NEXT_PUBLIC_BASE_URL_FE!),
   title: "Al-Birr Indonesia",
   description: "A local head company from Yogyakarta, Indonesia",
+  keywords: ["Al-Birr Indonesia", "Al-Birr Klambi","AB T-Shirt","M66 T-Shirt","Al-Birr Transportation","Transportasi Murah Jogja","City Tour Jogja","Oleh-Oleh murah jogja","Oleh-oleh khas jogja"],
+  openGraph:{
+    images:["/opengraph-image.jpg"]
+  }
 };
 
 export default function RootLayout({
