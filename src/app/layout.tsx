29cc5cb469c00199396401b9d7dbb5e06a05ea30
irfanhidayatullah <irfanhidayatullah.jobs@gmail.com>
2024-07-18 02:17:21 +0700

    code challenge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import { Providers } from "@/providers/ChakraProviders";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
